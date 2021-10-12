068f98e281a7f4afe308afe88d6b1ad3d450364a
Ashutosh Kr Pandey <92009208+AshutoshPandey05@users.noreply.github.com>
2021-10-12 22:08:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,24 @@
-﻿using System;
+﻿
 
-namespace Demo_LatestlangFeatures
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            coordinates India = new coordinates(120, 150);
-            Console.WriteLine("India coordinates r:" + India.X + India.Y);
-        }
-    }
+public class Class1
+		{
+			public Class1()
+			{
+				static void Main(string[] args)
+				{
+					Console.WriteLine("Hello World!!");
+					Console.WriteLine("Enter Your name..!!");
+					string name = Console.ReadLine();
 
-    public record person(string Fname, string Lname);
-    public record person1
-    {
-        public string Firstname { get; init; } = default!;
-        public string lastname { get; init; } = default!;
+					Console.WriteLine("Enter Your Email Address");
+					string Email = Console.ReadLine();
 
-    }
-    public record Newperson
-    {
-        public string Firstname { get; set; } = default!;
-        public string lastname { get; set; } = default!;
-    }
-}
+					Console.WriteLine("Enter Your Mob Number");
+					var Mob = Convert.ToInt32(Console.ReadLine());
+				}
+			}
+		}
+
+	
+
 
