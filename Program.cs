839c768f419f5683b18d9142bc23178de728bd63
Ashutosh Kr Pandey <92009208+AshutoshPandey05@users.noreply.github.com>
2021-10-13 14:27:36 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿
+﻿using System;
+using System.IO;
 
-public class Class1
-		{
-			public Class1()
-			{
-				static void Main(string[] args)
-				{
-					Console.WriteLine("Hello World!!");
-					Console.WriteLine("Enter Your name..!!");
-					string name = Console.ReadLine();
-
-					Console.WriteLine("Enter Your Email Address");
-					string Email = Console.ReadLine();
-
-					Console.WriteLine("Enter Your Mob Number");
-					var Mob = Convert.ToInt32(Console.ReadLine());
-				}
-			}
-		}
-
-	
-
-
+namespace Files_handlings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("File_Handling.......!!");
+            FileStream fs = new FileStream("D:\\Repos File\\MyFile.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("We Can Read and Display From File");
+            string str = sr.ReadToEnd();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+        }
+    }
+}
